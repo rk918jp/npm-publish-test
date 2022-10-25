56c469f95f20c95f5c1fa96e20b81bc9572b4788
rk918jp <rk918jp@users.noreply.github.com>
2022-10-25 12:10:25 +0900

    build with emotion

--- sample-module/src/Button/index.jsx
+++ sample-module/src/Button/index.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import {Button} from "@mui/material";
+import styled from "@emotion/styled";
+
+const StyledButton = styled(Button)`
+  color: red;
+`
 
 export const MyButton = (props) => {
   return (
-    <Button {...props} />
+    <StyledButton {...props} />
   )
 }
